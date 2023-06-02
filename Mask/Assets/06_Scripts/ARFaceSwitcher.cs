a55cf4e86d1ca95eea3ae594aaca8f6225ff5753
DESKTOP-ENKJ8RI\602 <greenleaf035@knou.ac.kr>
2023-06-02 19:58:30 +0900

    20230602_Mask 재질 변경_commit & push

--- Mask/Assets/06_Scripts/ARFaceSwitcher.cs
+++ Mask/Assets/06_Scripts/ARFaceSwitcher.cs
@@ -1,26 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;          //라이브러리 호출
+using UnityEngine.XR.ARFoundation;          //자동 라이브러리 호출
 
 public class ARFaceSwitcher : MonoBehaviour
 {
-    private ARFaceManager arFaceManager;    //ARFaceManager에서 Alt + Enter로 UnityEngine.XR.ARFoundation 라이브러리 호출
-    private Material currentMaterial;       //현 선택된 재질 저장
+    private ARFaceManager arFaceManager;    //AR Face Manager (script) - 자료형에서 Alt + Enter로 UnityEngine.XR.ARFoundation 라이브러리 호출
+    private Material currentMaterial;       //현재 재질
+
 
     private void Awake()
     {
         //초깃값
-        arFaceManager = GetComponent<ARFaceManager>();                                     //스크립트 저장. ARSessionOrigin의 컴포넌트에 있음
-        currentMaterial = arFaceManager.facePrefab.GetComponent<MeshRenderer>().material;  //초기 재질값 저장. AR Face Manager - FacePrefab - AR Default Face - MeshRenderer - material 
-
+        arFaceManager = GetComponent<ARFaceManager>();                                     //스크립트 . ARSessionOrigin의 컴포넌트에 있음
+        currentMaterial = arFaceManager.facePrefab.GetComponent<MeshRenderer>().sharedMaterial;  //AR Face Manager (script) - FacePrefab - AR Default Face - MeshRenderer - material 
     }
 
 
-    //버튼 OnClick이벤트
-    public void UpdateFaceMaterial(Material material)    //(버튼 클릭에 따라) 재질 변경 함수
+
+    public void UpdateFaceMaterial(Material _material)    //(버튼 클릭에 따라) 재질 변경
     {
-        currentMaterial = material;
+        currentMaterial = _material;
     }
 
 
@@ -28,12 +28,13 @@ public class ARFaceSwitcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach (ARFace face in arFaceManager.trackables) 
+        //매 프레임마다 마스크 인식
+        foreach (ARFace face in arFaceManager.trackables) //AR Face Manager (script)에서 추적 중인 값을 face에 저장하는 걸 반복
         {
-            face.GetComponent<MeshRenderer>().material = currentMaterial;
+            face.GetComponent<MeshRenderer>().sharedMaterial = currentMaterial; // 저장된 face값 - MeshRenderer - material 을 현재 재질로 변경
 
         }
 
-        
+
     }
 }
